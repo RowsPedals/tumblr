6893d67c44817c8365b829abe1a4a068dc3bfe6e
RowsPedals <khakibeige3@gmail.com>
2023-03-07 15:36:42 -0500

    added a bit of js for the *more* button

---
+++ script.js
@@ -0,0 +1,8 @@
+function showTheThing (){
+    var theThing = document.getElementById('extras');
+    if (theThing.style.display === 'none') {
+        theThing.style.display = 'block';
+    } else {
+        theThing.style.display = 'none';
+    }
+}
